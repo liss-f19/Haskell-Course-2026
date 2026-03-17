8b58f77760a8e50cda88b103996f3d14040dec4a
liss-f19 <01183028@pw.edu.pl>
2026-03-17 16:41:23 +0100

    tasks 1-5 working

---
+++ homeworks/hw1/Solution.hs
@@ -0,0 +1,93 @@
+import Data.Function
+import Data.Time.Format.ISO8601 (yearFormat)
+import Data.List (permutations)
+
+-- from Basics/src/Tutorials01.hs
+isPrime :: Int -> Bool
+isPrime n 
+ | n <= 1 = False
+ | n == 2 = True
+ | even n = False -- not needed but, makes the function run faster
+ | otherwise = all (/= 0) [ n `mod` i | i <- [2..s]] --  n is not div by i
+  where 
+    s = n & fromIntegral & sqrt & floor -- funky way of writing 
+                                        -- s = floor $ sqrt $ fromIntegral n
+
+-- Task 1. Goldbach pairs
+goldbachPairs :: Int -> [(Int, Int)]
+goldbachPairs n = 
+  [ (p,q)
+  | p <- [2..n]
+  , q <- [p..n-p]
+  , p + q == n
+  , isPrime p
+  , isPrime q
+  ]  
+
+
+-- Task 2. Coprime pairs
+-- Note: conventionally xs is a list[values]
+coprimePairs :: [Int] -> [(Int, Int)]
+coprimePairs xs = 
+  [
+    (x,y)
+  | x <- xs
+  , y <- xs
+  , x < y
+  , gcd x y == 1
+  ]
+
+
+-- Task 3. Sieve of Eratosthenes
+sieve :: [Int] -> [Int]
+sieve [] = [] --base case
+sieve (p:xs) = p:sieve[x|x<-xs, x `mod` p /= 0]
+
+primesTo :: Int -> [Int]
+primesTo n = sieve [2..n]
+
+isPrimeSieve :: Int -> Bool
+isPrimeSieve n
+ | n < 2 = False
+ | otherwise = n `elem` primesTo n
+
+
+-- Task 4. Matrix multiplication
+-- Helper for checking the number of rows in matrix is the same
+allEqual :: [[a]] -> Bool
+allEqual xs = all ((== length (head xs)) . length) xs
+
+
+
+matMul :: [[Int]] -> [[Int]] -> [[Int]]
+matMul a b
+ | null a || null b = error "Matrices cant be empty"
+ | not (allEqual a) = error "Matrix a - all rows should have the same length"
+ | not (allEqual b) = error "Matrix b - all rows should have the same length"
+ | length(head a) /= length b = error "Number of columns of matrix a should be == number of rows of matrix b"
+ | otherwise = 
+  let m = length a
+      p = length(head a)
+      n = length(head b)
+  in [[sum [ a !! i !! k * b !! k !! j | k <- [0 .. p-1] ]
+      | j <- [0..n-1]]
+      | i <- [0..m-1]]
+
+
+-- Task 5. Permutations
+-- Helper
+remove :: Eq a => a -> [a] -> [a]
+remove _ [] = []
+remove y (z:zs)
+  | y == z    = zs
+  | otherwise = z : remove y zs
+
+-- Generate all k-element permutations
+permutationsCustom :: Eq a => Int -> [a] -> [[a]]
+permutationsCustom 0 _  = [[]]  -- base case
+permutationsCustom _ [] = []
+permutationsCustom k xs = 
+  [ x : ps
+  | x <- xs
+  , ps <- permutationsCustom (k-1) (remove x xs)
+  ]
